5f66b6da89def68cd7e8072c88062d897312238b
Eddiksyre <125164155+Eddiksyre@users.noreply.github.com>
2023-04-11 10:54:11 +0100

    shopping cart saving
    saves the submitted shopping cart from the menu page. Saves it in order.json in root folder

---
+++ Project/project/springboot/demo/src/main/java/com/example/demo/OrderController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/api/order")
+    public void placeOrder(@RequestBody Order order) {
+        System.out.println("Received order: " + order.getItems());
+        orderService.saveOrder(order);
+    }
+
+}
